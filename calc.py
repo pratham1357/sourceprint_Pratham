66fc5e3767fa6d029ec9582a1e670b1de535144b
pratham1357 <53139872+pratham1357@users.noreply.github.com>
2023-09-23 14:48:52 +0530

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,9 @@
+#Hum Sourceprint mein calculator bana rhe hai 
+a = eval(input("Enter first no.: "))
+b = eval(input("Enter second number.: "))
+
+sum1 = a+b
+diff = a-b
+print(sum1)
+print(diff)
+print("This is a calculator!")
